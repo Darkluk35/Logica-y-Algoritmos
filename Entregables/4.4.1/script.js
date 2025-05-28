70b2f369cf53e56f2e03c8daacc4c5ff0d3d21da
Luis <jesuslara.luis99@gmail.com>
2025-05-27 21:08:25 -0600

    Se agrega la funcion para encontrar el numero minimo

--- Entregables/4.4.1/script.js
+++ Entregables/4.4.1/script.js
@@ -16,6 +16,33 @@ function findMax(arr) {
     // TODO: Combinar las soluciones comparando los máximos
     return Math.max(leftMax,rightMax);
 }
+
+
+function findMin(arr) {
+    // TODO: Agregar la condición del caso base
+    if (arr.length === 1) {
+        return arr[0];
+    }
+
+    // TODO: Dividir el arreglo en dos mitades
+    const mid = Math.floor(arr.length/2)/* calcular el punto medio */;
+    const left = arr.slice(0,mid)/* obtener la primera mitad */;
+    const right = arr.slice(mid)/* obtener la segunda mitad */;
+
+    // TODO: Llamar recursivamente a la función para ambas mitades
+    const leftMin = findMin(left)/* llamada recursiva */;
+    const rightMin = findMin(right) /* llamada recursiva */;
+
+    // TODO: Combinar las soluciones comparando los máximos
+    return Math.min(leftMin,rightMin);
+}
+
+
 // Ejemplo de entrada
-const numbers = [3, 8, 2, 1, 5, 7];
-console.log(findMax(numbers)); // Salida esperada: 10
+const numbers = [3, 8, 2, 10, 5, 7];
+const numberMin = findMax(numbers)
+const numberMax = findMin(numbers)
+
+
+console.log(`El numero mínimo es: ${numberMin}`); // Salida esperada: 2
+console.log(`El numero máximo es: ${numberMax}`);// Salida esperada: 10
